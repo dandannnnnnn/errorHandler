3f3bf37f1f5d1cf36e6bc86d764c6c0618932924
dandannnnnnn <dandansandan04@gmail.com>
2024-12-04 09:19:31 +0100

    topic and port change

--- Message_Handler.c
+++ Message_Handler.c
@@ -3,9 +3,9 @@
 #include <MQTTClient.h>
 
 
-#define ADDRESS     "tcp://192.168.0.101:1883"       // MQTT broker URL
+#define ADDRESS     "tcp://192.168.0.101:1880"       // MQTT broker URL
 #define CLIENTID    "RaspberryPiClient"             // Unique client ID
-#define TOPIC       "test/topic"                    // MQTT topic
+#define TOPIC       "topic2"                    // MQTT topic
 #define PAYLOAD     "testing"      // Message to send
 #define QOS         1                               // Quality of Service level
 #define TIMEOUT     10000L                          // Timeout in milliseconds
